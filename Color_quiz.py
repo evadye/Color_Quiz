c239cdb1b870439b0478b8c9a16b01d5035e3c14
evadye <88847497+evadye@users.noreply.github.com>
2021-08-20 19:59:02 -0700

    Update Color_quiz.py

--- Color_quiz.py
+++ Color_quiz.py
@@ -1,5 +1,5 @@
 # Eva Dye
-# August 12th, 2021
+# August 11th, 2021
 # Python 2.7
 # Python program for a color guess game.
 
@@ -11,7 +11,7 @@ color_answers = ["Red", "red", "Orange", "orange", "Yellow", "yellow", "Green", 
 question_answers = positive_answers + negative_answers
 
 # Message library
-victory_message = "Woo-hoo! Aren't colors wonderful?"
+victory_message = "Woo-hoo! Thanks for playing!"
 defeat_message = "Drat. I need to study the color wheel better..."
 
 
@@ -37,91 +37,111 @@ opening()
 
 def game():
     starting_question = raw_input("Is your color a warm color? ")
-    # Warm colors answer tree
-    if starting_question in positive_answers:
-        warm_question_one = raw_input("Is your color a primary color? ")
-        if warm_question_one in positive_answers:
-            # 'Red' answer path
-            warm_question_two = raw_input("Can your color be used to make purple? ")
-            if warm_question_two in positive_answers:
-                warm_question_three = raw_input("Is your color red? ")
-                if warm_question_three in positive_answers:
-                    print victory_message
-                elif warm_question_three in negative_answers:
-                    end_question_red = raw_input("What was your color? ")
-                    if end_question_red in color_answers:
-                        print defeat_message
-            # 'Yellow' answer path
-            elif warm_question_two in negative_answers:
-                warm_question_four = raw_input("Can your color be used to make green? ")
-                if warm_question_four in positive_answers:
-                    warm_question_five = raw_input("Is your color yellow? ")
-                    if warm_question_five in positive_answers:
-                        print victory_message
-                elif warm_question_four in negative_answers:
-                    end_question_yellow = raw_input("What was your color? ")
-                    if end_question_yellow in color_answers:
-                        print defeat_message
-        # Orange answer path
-        elif warm_question_one in negative_answers:
-            warm_question_six = raw_input("Is your color also a fruit? ")
-            if warm_question_six in positive_answers:
-                warm_question_seven = raw_input("Is your color orange? ")
-                if warm_question_seven in positive_answers:
-                    print victory_message
-            elif warm_question_six in negative_answers:
-                end_question_orange = raw_input("What was your color? ")
-                if end_question_orange in color_answers:
-                    print defeat_message
-    # Cool colors answer tree
-    elif starting_question in negative_answers:
-        cool_question_one = raw_input("Is your color a cool color? ")
-        if cool_question_one in positive_answers:
-            cool_question_two = raw_input("Is your color a primary color? ")
-            if cool_question_two in positive_answers:
-                # 'Blue' answer path
-                cool_question_three = raw_input("Can your color be used to make purple? ")
-                if cool_question_three in positive_answers:
-                    cool_question_four = raw_input("Is your color blue? ")
-                    if cool_question_four in positive_answers:
-                        print victory_message
-                    elif cool_question_four in negative_answers:
-                        end_question_blue = raw_input("What was your color? ")
-                        if end_question_blue in color_answers:
-                            print defeat_message
-            # Green answer path
-            elif cool_question_two in negative_answers:
-                cool_question_five = raw_input("Is your color the same color as grass? ")
-                if cool_question_five in positive_answers:
-                    cool_question_six = raw_input("Is your color green? ")
-                    if cool_question_six in positive_answers:
-                        print victory_message
-                    elif cool_question_six in negative_answers:
-                        end_question_green = raw_input("What was your color? ")
-                        if end_question_green in color_answers:
-                            print defeat_message
-                # Indigo answer path
-                elif cool_question_five in negative_answers:
-                    cool_question_eight = raw_input("Is your color often mistaken for blue or purple? ")
-                    if cool_question_eight in positive_answers:
-                        cool_question_nine = raw_input("Is your color indigo? ")
-                        if cool_question_nine in positive_answers:
-                            print victory_message
-                        elif cool_question_nine in negative_answers:
-                            end_question_indigo = raw_input("What was your color? ")
-                            if end_question_indigo in color_answers:
-                                print defeat_message
-                    # Purple answer path
-                    elif cool_question_eight in negative_answers:
-                        cool_question_ten = raw_input("Is your color the same color as an eggplant? ")
-                        if cool_question_ten in positive_answers:
-                            cool_question_eleven = raw_input("Is your color purple? ")
-                            if cool_question_eleven in positive_answers:
-                                print victory_message
-                        elif cool_question_ten in negative_answers:
-                            end_question_purple = raw_input("What was your color? ")
-                            if end_question_purple in color_answers:
-                                print defeat_message
+    while True:
+        # Warm colors answer tree
+        if starting_question in positive_answers:
+            def warm_question_1():
+                warm_question_one = raw_input("Is your color a primary color? ")
+                while True:
+                    if warm_question_one in positive_answers:
+                        # 'Red' answer path
+                        def warm_question_2():
+                            warm_question_two = raw_input("Can your color be used to make purple? ")
+                            while True:
+                                if warm_question_two in positive_answers:
+                                    def warm_question_3():
+                                        warm_question_three = raw_input("Is your color red? ")
+                                        while True:
+                                            if warm_question_three in positive_answers:
+                                                print victory_message
+                                            elif warm_question_three in negative_answers:
+                                                def end_question_r():
+                                                    end_question_red = raw_input("What was your color? ")
+                                                    while True:
+                                                        if end_question_red in color_answers:
+                                                            print defeat_message
+                                                        elif end_question_red not in color_answers:
+                                                            print "Please enter a ROY G BIV color."
+                                                            return end_question_r()
+                                            elif warm_question_three not in question_answers:
+                                                print "Please enter valid input."
+                                                return warm_question_3()
+                                # 'Yellow' answer path
+                                elif warm_question_two in negative_answers:
+                                    def warm_question_4():
+                                        warm_question_four = raw_input("Can your color be used to make green? ")
+                                        while True:
+                                            if warm_question_four in positive_answers:
+                                                def warm_question_5():
+                                                    warm_question_five = raw_input("Is your color yellow? ")
+                                                    while True:
+                                                        if warm_question_five in positive_answers:
+                                                            print victory_message
+                                                        elif warm_question_five in negative_answers:
+                                                            def end_question_y():
+                                                                end_question_yellow = raw_input("What was your color? ")
+                                                                while True:
+                                                                    if end_question_yellow in color_answers:
+                                                                        print defeat_message
+                                                                    elif end_question_yellow not in color_answers:
+                                                                        print "Please enter a ROY G BIV color."
+                                                                        return end_question_y()
+                                                        else:
+                                                            print "Invalid input."
+                                                            return warm_question_5()
+                                            elif warm_question_four in negative_answers:
+                                                def end_question_y2():
+                                                    end_question_yellow = raw_input("What was your color? ")
+                                                    while True:
+                                                        if end_question_yellow in color_answers:
+                                                            print defeat_message
+                                                        elif end_question_yellow not in color_answers:
+                                                            print "Invalid input. Please enter a ROY G BIV color."
+                                                            return end_question_y2()
+                                            else:
+                                                print "Invalid input."
+                                                return warm_question_4()
+                                else:
+                                    print "Invalid input."
+                                    return warm_question_2()
+                # Orange answer path
+                    elif warm_question_one in negative_answers:
+                        def warm_question_6():
+                            warm_question_six = raw_input("Is your color also a fruit? ")
+                            while True:
+                                if warm_question_six in positive_answers:
+                                    def warm_question_7():
+                                        warm_question_seven = raw_input("Is your color orange? ")
+                                        while True:
+                                            if warm_question_seven in positive_answers:
+                                                print victory_message
+                                            elif warm_question_seven in negative_answers:
+                                                def end_question_o():
+                                                    end_question_orange = raw_input("What was your color? ")
+                                                    while True:
+                                                        if end_question_orange in color_answers:
+                                                            print defeat_message
+                                                        elif end_question_orange not in color_answers:
+                                                            print "Invalid input. Please enter a ROY G BIV color."
+                                                            return end_question_o()
+                                            else:
+                                                print "Invalid input."
+                                                return warm_question_7()
+                                elif warm_question_six in negative_answers:
+                                    def end_question_o2():
+                                        end_question_orange = raw_input("What was your color? ")
+                                        while True:
+                                            if end_question_orange in color_answers:
+                                                print defeat_message
+                                            elif end_question_orange not in color_answers:
+                                                print "Invalid input. Please enter a ROY G BIV color."
+                                                return end_question_o2()
+                                else:
+                                    print "Invalid input."
+                                    return warm_question_6()
+                    else:
+                        print "Invalid input."
+                        return warm_question_1()
 
 
 game()
@@ -139,5 +159,6 @@ def play_again():
         elif answer not in question_answers:
             print "Please enter valid input."
             return play_again()
+
 
 play_again()
